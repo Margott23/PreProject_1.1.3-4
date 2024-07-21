e07895d95ebda26f1eeb88b65479bc7faebcad12
Margott23 <margott23@mail.ru>
2024-07-21 18:18:58 +0300

    feature: add transaction for saveUser, edits

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,36 +3,31 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import javax.transaction.Transaction;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class UserDaoJDBCImpl implements UserDao {
 
-    private final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `users` " +
+    private String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `users` " +
             "(`id` INT NOT NULL AUTO_INCREMENT, " +
             "`name` VARCHAR(45) NULL, " +
             "`lastName` VARCHAR(45) NULL, " +
             "`age` INT NULL, PRIMARY KEY (`id`));";
-    private final String DROP_TABLE = "DROP TABLE IF EXISTS users";
-    private final String SAVE_USER = "INSERT INTO users (name, lastName, age) VALUES (?,?,?)";
-    private final String REMOVE_USER_BY_ID = "DELETE FROM `users` WHERE `id` = ?";
-    private final String GET_ALL_USERS = "SELECT * FROM `users`";
-    private final String CLEAN_USERS_TABLE = "DELETE FROM `users`";
-    private final String CHECK_TABLE = "SHOW TABLES LIKE 'users'";
+    private String DROP_TABLE = "DROP TABLE IF EXISTS users";
+    private String SAVE_USER = "INSERT INTO users (name, lastName, age) VALUES (?,?,?)";
+    private String REMOVE_USER_BY_ID = "DELETE FROM `users` WHERE `id` = ?";
+    private String GET_ALL_USERS = "SELECT * FROM `users`";
+    private String CLEAN_USERS_TABLE = "DELETE FROM `users`";
 
     public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
         try (PreparedStatement preparedStatement = Util.getInstance().getConnection().prepareStatement(CREATE_TABLE)) {
-            preparedStatement.executeUpdate(CREATE_TABLE);
-            System.out.println("Table created successfull");
+            preparedStatement.executeUpdate();
+            System.out.println("Table created successful");
         } catch (SQLException e) {
             System.out.println("UserDaoJDBCImpl error: method createUsersTable");
         }
@@ -41,21 +36,28 @@ public class UserDaoJDBCImpl implements UserDao {
     public void dropUsersTable() {
         try (PreparedStatement preparedStatement = Util.getInstance().getConnection().prepareStatement(DROP_TABLE)) {
             preparedStatement.executeUpdate();
-            System.out.println("Table drop successfull");
+            System.out.println("Table drop successful");
         } catch (SQLException e) {
             System.out.println("UserDaoJDBCImpl error: method dropUsersTable" + e.getMessage());
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement preparedStatement = Util.getInstance().getConnection().prepareStatement(SAVE_USER)) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            System.out.printf("User %s successfull added\n", name);
+        try (Connection connection = Util.getInstance().getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(SAVE_USER)) {
+                connection.setAutoCommit(false);
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
+                preparedStatement.executeUpdate();
+                System.out.printf("User %s successful added\n", name);
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                System.out.println("UserDaoJDBCImpl error: method saveUser");
+            }
         } catch (SQLException e) {
-            System.out.println("UserDaoJDBCImpl error: method saveUser");
+            System.out.println("UserDaoJDBCImpl error: method saveUser lost connection");
         }
     }
 
@@ -63,7 +65,7 @@ public class UserDaoJDBCImpl implements UserDao {
         try (PreparedStatement preparedStatement = Util.getInstance().getConnection().prepareStatement(REMOVE_USER_BY_ID)) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
-            System.out.println("User removed successfull");
+            System.out.println("User removed successful");
         } catch (SQLException e) {
             System.out.println("UserDaoJDBCImpl error: method removeUserById");
         }
@@ -83,7 +85,7 @@ public class UserDaoJDBCImpl implements UserDao {
         } catch (SQLException e) {
             System.out.println("UserDaoJDBCImpl error: method getAllUsers");
         }
-        System.out.println("Get all users successfull");
+        System.out.println("Get all users successful");
         System.out.println(users);
         return users;
     }
@@ -91,7 +93,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (PreparedStatement preparedStatement = Util.getInstance().getConnection().prepareStatement(CLEAN_USERS_TABLE)) {
             preparedStatement.execute();
-            System.out.println("Table clean successfull");
+            System.out.println("Table clean successful");
         } catch (SQLException e) {
             System.out.println("UserDaoJDBCImpl error: method cleanUsersTable");
         }
